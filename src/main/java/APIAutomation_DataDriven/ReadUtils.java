846e7c359decfba0d343aeee8bbbae846d81ec6e
DebdipAutomate <debdiproy95@gmail.com>
2022-02-20 16:39:32 +0530

    ReadUtils class added

---
+++ src/main/java/APIAutomation_DataDriven/ReadUtils.java
@@ -0,0 +1,12 @@
+package APIAutomation_DataDriven;
+
+import org.testng.annotations.Test;
+
+public class ReadUtils {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("this is to test git");
+    }
+}
